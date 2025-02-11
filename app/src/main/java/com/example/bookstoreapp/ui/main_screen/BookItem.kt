b0df86c19400997aa7630db1de5ae1116e44351a
Any1CaNDo <maksimchik2003@bk.ru>
2025-02-11 11:24:24 +0300

    Создание BookListItem

--- app/src/main/java/com/example/bookstoreapp/ui/main_screen/BookItem.kt
+++ app/src/main/java/com/example/bookstoreapp/ui/main_screen/BookItem.kt
@@ -1,14 +1,58 @@
 package com.example.bookstoreapp.ui.main_screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.bookstoreapp.R
 
+@Preview
 @Composable
 fun BookItem() {
     Column(
-        modifier = Modifier
+        modifier = Modifier.fillMaxWidth().padding(16.dp)
     ) {
-
+        AsyncImage(
+            model = R.drawable.test_image,
+            contentDescription = "TestImage",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(220.dp)
+                .clip(RoundedCornerShape(16.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = "Mass Effect",
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = "Description",
+            color = Color.Gray,
+            fontSize = 16.sp
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = "50 dollars",
+            color = Color.Blue,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp
+        )
     }
 }
